67a9ce549da41c56179f3d98b07288eec26aca1f
Tomas Korcak <korczis@gmail.com>
2014-01-21 01:19:06 +0100

    Fixed config mongo uri

--- config.js
+++ config.js
@@ -32,7 +32,7 @@
 
             verbose: true,
             mongo: {
-                uri: "mongodb://localhost:27017/apollo"
+                uri: "mongodb://localhost:27017/data"
             },
 
             server: {

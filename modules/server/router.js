7f8f3f1bc552e5b3d60c562f20c6308e1040f4fe
Tomas Korcak <korczis@gmail.com>
2014-01-21 16:19:18 +0100

    Better search ...

--- modules/server/router.js
+++ modules/server/router.js
@@ -60,6 +60,8 @@
         app.get('/query', function(req, res) {
             var col = microscratch.mongo.getCollection('datasets').then(function(coll) {
                 var q = req.query.q || "";
+                q = q.replace(" ", ".*");
+                
                 coll.find({'value.data.name': new RegExp(q, "i")}).limit(10).toArray(function (err, data) {
                    res.json(data);
                 });

b7a7ac7b49da218b0ad47dfb42c376fd27cd03ee
Tomas Korcak <korczis@gmail.com>
2014-01-26 16:53:21 +0100

    Placeholder data

--- public/app/routes/application.js
+++ public/app/routes/application.js
@@ -25,19 +25,6 @@
     (
         ["ember", "app"], function (Ember, App) {
 
-            App.ApplicationRoute = Ember.Route.extend({
-                title: "Microscratch",
-
-                model: function() {
-                },
-
-                setupController: function(controller, model) {
-                }
-            });
-
-            App.ApplicationController = Ember.Controller.extend({
-            });
-
             App.ApplicationView = Ember.View.extend({
                 classNames: ['app-view'],
                 templateName: "application",
@@ -50,6 +37,76 @@
                 didInsertElement: function () {
                     console.log("App.ApplicationView.didInsertElement()");
                 }
+            });
+
+            App.IndexRoute = Ember.Route.extend({
+                model: function() {
+                    return [{
+                        "_id": "51bbba16482e431e98002f08",
+                        "value": {
+                            "data": {
+                                "address": {},
+                                "categories": [],
+                                "createdAt": "2013-06-22 00:15:59 UTC",
+                                "name": " ARCH ESTATES, s.r.o. ",
+                                "profileUrl": "http://www.firmy.cz/detail/12728186-arch-estates-praha-nove-mesto.html",
+                                "updatedAt": "2013-06-22 00:15:59 UTC"
+                            },
+                            "dataset": "companies"
+                        }
+                    },
+                        {
+                            "_id": "51bf777f482e431278000410",
+                            "value": {
+                                "data": {
+                                    "address": {},
+                                    "categories": [],
+                                    "createdAt": "2013-07-06 13:14:17 UTC",
+                                    "name": " Adéla Kopecká ",
+                                    "profileUrl": "http://www.firmy.cz/detail/628486-adela-kopecka-praha-podoli.html",
+                                    "updatedAt": "2013-07-06 13:14:17 UTC"
+                                },
+                                "dataset": "companies"
+                            }
+                        },
+                        {
+                            "_id": "51be9546482e431f24003485",
+                            "value": {
+                                "data": {
+                                    "address": {},
+                                    "categories": [],
+                                    "createdAt": "2013-07-04 01:11:43 UTC",
+                                    "name": " Animalia, s.r.o. ",
+                                    "profileUrl": "http://www.firmy.cz/detail/1968392-animalia-praha-vinohrady.html",
+                                    "updatedAt": "2013-07-04 01:11:43 UTC"
+                                },
+                                "dataset": "companies"
+                            }
+                        },
+                        {
+                            "_id": "51bcbd8a482e431e98008373",
+                            "value": {
+                                "data": {
+                                    "address": {},
+                                    "categories": [],
+                                    "createdAt": "2013-06-23 04:45:10 UTC",
+                                    "name": " BONDTOUR, s.r.o. ",
+                                    "profileUrl": "http://www.firmy.cz/detail/312259-bondtour-praha-zizkov.html",
+                                    "updatedAt": "2013-06-23 04:45:10 UTC"
+                                },
+                                "dataset": "companies"
+                            }
+                        }
+                    ];
+                },
+
+                setupController: function(controller, model) {
+                    controller.set('results', model);
+                }
+            });
+
+            App.IndexController = Ember.Controller.extend({
+                results: Ember.A([])
             });
         }
     );
